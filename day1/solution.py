687238af12f5ecbe352710bc9770c749c6554ec7
Valentin Iliev <justwannaball47@gmail.com>
2025-12-01 15:13:36 +0200

    day1 finished

---
+++ day1/solution.py
@@ -0,0 +1,23 @@
+with open(r'./day1/input.txt','r') as f:
+    commands = f.read().split('\n')
+
+start = 50
+times = 0
+current = start
+for command in commands:
+    print(current,command)
+    direction = command[0]
+    moves = int(command[1:])
+    number = moves*(-1 if direction=='L' else 1)
+    current = current + number
+    while current < 0:
+        current = 100 + current
+    while current >= 100:
+        current = current - 100
+    print(current)
+    if current == 0:
+        times +=1
+        # break
+
+
+print(times)
