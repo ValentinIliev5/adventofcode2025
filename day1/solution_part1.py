ddfa996d5e34ebc25bc97164c215f4c35d145a94
Valentin Iliev <justwannaball47@gmail.com>
2025-12-01 15:31:26 +0200

    restructuring + begin part 2

--- day1/solution_part1.py
+++ day1/solution_part1.py
@@ -1,11 +1,11 @@
-with open(r'./day1/input.txt','r') as f:
+with open(r'./day1/input_part1.txt','r') as f:
     commands = f.read().split('\n')
 
 start = 50
 times = 0
 current = start
 for command in commands:
-    print(current,command)
+    # print(current,command)
     direction = command[0]
     moves = int(command[1:])
     number = moves*(-1 if direction=='L' else 1)
@@ -14,7 +14,7 @@ for command in commands:
         current = 100 + current
     while current >= 100:
         current = current - 100
-    print(current)
+    # print(current)
     if current == 0:
         times +=1
         # break
